412dc2823e1d39e56d750a8d87d8de09fda6e582
Jason Stajich <jason.stajich@ucr.edu>
2016-06-26 17:10:55 -0700

    FPKM for feature

--- expression/make_norm_csv.R
+++ expression/make_norm_csv.R
@@ -1,8 +1,10 @@
 tbl <- read.table("Cimmitis_RS_Genome.rpt.bedtools.tab",header=F,sep="\t")
-len <- abs(tbl$V3-tbl$V2) / 1000
+len <- abs(tbl$V3-tbl$V2) 
 # hyphae numbers, lib ssize is 43.3M
-tbl$V9 <- (tbl$V7 / len) * 43.3
-# spherule numbers, lib size is 36.7
-tbl$V10 <- (tbl$V8 / len) * 36.7
-colnames(tbl) <- c("Scaffold","ChrStart","ChrEnd","TE_name_instance","RepeatMasker Score","Strand","Hyphae Expression", "Spherule Expression","Hyphae Expression Normalized","Spherule Expression Normalized")
+tbl$V9 <- (10^9 * tbl$V7) / (43323438 * len) 
+# spherule numbers, lib size is 36.7M
+tbl$V10 <- (10^9 * tbl$V8) / (36703973 * len)
+# feature length
+tbl$V11 <- len
+colnames(tbl) <- c("Scaffold","ChrStart","ChrEnd","TE_name_instance","RepeatMasker Score","Strand","Hyphae Expression", "Spherule Expression","Hyphae Expression (FPKM)","Spherule Expression (FPKM)","Feature Length (bp)")
 write.csv(tbl,file="Cimmitis_RS_Genome.rpt.bedtools.csv")
