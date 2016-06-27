deb2a1d4d5d70b6ed2877ba79660a0dd097a92b2
Jason Stajich <jason.stajich@ucr.edu>
2016-06-26 22:13:32 -0700

    sort by expression columns and remove rownames from output

--- expression/make_norm_csv.R
+++ expression/make_norm_csv.R
@@ -1,12 +1,13 @@
 tbl <- read.table("Cimmitis_RS_Genome.rpt.bedtools.tab",header=F,sep="\t")
 len <- abs(tbl$V3-tbl$V2) 
 # hyphae numbers, lib ssize is 43.3M
-tbl$V9 <- (10^9 * tbl$V7) / (43323438 * len) 
+tbl[,9] <- (10^9 * tbl$V7) / (43323438 * len)
 # spherule numbers, lib size is 36.7M
-tbl$V10 <- (10^9 * tbl$V8) / (36703973 * len)
+tbl[,10] <- (10^9 * tbl$V8) / (36703973 * len)
 # feature length
-tbl$V11 <- len
+tbl[,11] <- len
+summary(tbl)
 tbl<-subset(tbl,tbl$V11 > 10)
 colnames(tbl) <- c("Scaffold","ChrStart","ChrEnd","TE_name_instance","RepeatMasker Score","Strand","Hyphae Expression", "Spherule Expression","Hyphae Expression (FPKM)","Spherule Expression (FPKM)","Feature Length (bp)")
-
-write.csv(tbl,file="Cimmitis_RS_Genome.rpt.bedtools.csv")
+tbl <- tbl[order(-tbl[,10],-tbl[,9]),]
+write.csv(tbl,file="Cimmitis_RS_Genome.rpt.bedtools.csv",row.names=FALSE)
